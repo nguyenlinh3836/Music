eaee4da95690dbc5439b5bcc4e7f82f6b6013065
phuong2001 <54660042+phuong2001@users.noreply.github.com>
2020-12-08 14:51:21 +0700

    1

---
+++ MusicWorld/Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicWorld.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            MusicDbContext context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<MusicDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            if (!context.Genres.Any())
+            {
+                context.Genres.AddRange(
+                        new Genre
+                        {
+                            GenreID= 1,
+                            GenreName= "Rock"
+                        },
+                        new Genre
+                        {
+                            GenreID = 2,
+                            GenreName = "Pop"
+                        },
+                        new Genre
+                        {
+                            GenreID = 3,
+                            GenreName = "Rap/Hip-Hop"
+                        },
+                        new Genre
+                        {
+                            GenreID = 4,
+                            GenreName = "EDM"
+                        },
+                         new Genre
+                         {
+                             GenreID = 5,
+                             GenreName = "Bolero"
+                         }
+                       
+                       );
+                context.SaveChanges();
+            }
+        }
+    }
+}
